6938e1063ed05eaf4270dc456b5a3ad7ed1b021c
Chris Leatham <chris13@uw.edu>
2017-04-13 13:42:03 -0700

    fix

--- js/index.js
+++ js/index.js
@@ -34,8 +34,6 @@ $('.tab a').on('click', function (e) {
   $(this).parent().addClass('active');
   $(this).parent().siblings().removeClass('active');
   
-  target = $(this).attr('href');
-
   $('.tab-content > div').not(target).hide();
   
   $(target).fadeIn(600);
